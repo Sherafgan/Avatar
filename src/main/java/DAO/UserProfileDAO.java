94f770c8ea3bbd48581b28c84f0ed79a5cc0c2cb
Sherafgan <kssfortis@gmail.com>
2016-03-01 22:12:40 +0300

    added diagrams and updated/new classes

---
+++ src/main/java/DAO/UserProfileDAO.java
@@ -0,0 +1,8 @@
+package DAO;
+
+/**
+ * Created by Kenneth, Nikolay, Sherafgan, Viktor on 3/1/16.
+ */
+public class UserProfileDAO {
+
+}
--- src/main/java/Servlets/AuthorizationServlet.java
+++ src/main/java/Servlets/AuthorizationServlet.java
@@ -4,4 +4,11 @@ package Servlets;
  * Created by Kenneth, Nikolay, Sherafgan, Viktor on 2/29/16.
  */
 public class AuthorizationServlet {
+    /**
+     * inv:
+     * */
+
+    /**
+     * pre:
+     * */
 }
